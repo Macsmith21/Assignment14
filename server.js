3ada442aae08a37a7f161f7eae6ce5c36156326c
MAC <mac@MACs-MacBook-Pro.local>
2024-03-24 19:21:40 -0400

    finish

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-
-// Serve static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
+});
+
+app.get('/',(req,res) => {
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.get('/api/crafts', (req, res) => {
